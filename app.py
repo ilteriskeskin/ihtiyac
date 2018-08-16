4a8545fb81069d7a20adf23387d31fe13a0bc052
Ali İlteriş Keskin <ilteriskesk20@gmail.com>
2018-08-16 12:28:45 +0300

    Kelime düzeltmeleri, kodlama yapısı düzeltme

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ from functools import wraps
 from datetime import datetime
 from flask import send_from_directory
 from forms import LoginForm, RegisterForm, NeedForm
-from models import Need, User
 
 # Kullanıcı Giriş Decorator'ı
 
@@ -23,6 +22,28 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'linuxdegilgnulinux'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/ilteriskeskin/Çalışma/ihtiyac/data.db'
 db = SQLAlchemy(app)
+
+# Kullanıcı Veritabanı
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name= db.Column(db.String(15), unique=True)
+    username = db.Column(db.String(15), unique=True)
+    email = db.Column(db.String(50), unique=True)
+    password = db.Column(db.String(25), unique=True)
+
+# Kullanıcı İhtiyaç Veritabanı
+
+class Need(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    content = db.Column(db.String(1000))
+    address = db.Column(db.String(1000))
+    email = db.Column(db.String(45))
+    phone = db.Column(db.Integer)
+    category = db.Column(db.String(40))
+    iban = db.Column(db.String(50))
+    created_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
 @app.route('/')
 def home():
